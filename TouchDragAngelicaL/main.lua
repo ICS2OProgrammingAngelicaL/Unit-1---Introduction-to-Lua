bfdd7c50b381bc15c376025f29f96d991eb063f6
AngelicaHome <angelica.lutkiewicz@ocsbstudent.ca>
2018-10-05 11:33:55 -0400

    home 1

--- TouchDragAngelicaL/main.lua
+++ TouchDragAngelicaL/main.lua
@@ -2,12 +2,10 @@
 -- Title : TouchDrag
 -- Name : Angelica L
 -- Course : ICS20/3C
--- This programn images that the user can interact with
+-- This programn displays images that the user can interact with
 -----------------------------------------------------------------------------------------
 
 -- Local variables
 local backgroundImage = display.newImageRect("Images/background.png", 2048, 1536)
-
-local sandwich = display.newImageRect("Images/GrilledCheese", 150, 150)
 
 local heart = display.newImageRect("Images/healthheart.png", 150, 150)
