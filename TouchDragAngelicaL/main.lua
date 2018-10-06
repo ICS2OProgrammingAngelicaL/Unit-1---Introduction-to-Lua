a7d854be0f15659ae0addee08d4e88b6408b1df1
AngelicaHome <angelica.lutkiewicz@ocsbstudent.ca>
2018-10-06 10:56:46 -0400

    Changed

--- TouchDragAngelicaL/main.lua
+++ TouchDragAngelicaL/main.lua
@@ -16,9 +16,14 @@ local star = display.newImageRect("Images/star.png", 150, 150)
 local starWidth = star.width 
 local starHeight = star.height
 
+local planet = display.newImageRect("Images/planet.png", 150, 153)
+local planetWidth = planet.width
+local planetHeight = planet.height
+
 -- boolean variables
 local alreadyTouchedHeart = false
 local alreadyTouchedStar = false
+local alreadyTouchedPlanet = false
 -----------------------------------------------------------------------------------------
 
 -- local functions
@@ -30,7 +35,7 @@ local alreadyTouchedStar = false
 local function HeartListener(touch)
 
 	if (touch.phase == "began") then
-		if (alreadyTouchedStar == false) then
+		if (alreadyTouchedStar == false and alreadyTouchedPlanet == false) then
 			alreadyTouchedHeart = true
 		end
 	end
@@ -43,6 +48,7 @@ local function HeartListener(touch)
 	if (touch.phase == "ended") then
 	   alreadyTouchedHeart = false
 	   alreadyTouchedStar = false
+	   alreadyTouchedPlanet = false
 	end
 end
 
@@ -53,7 +59,7 @@ end
 local function StarListener(touch)
 
 	if (touch.phase == "began") then
-		if (alreadyTouchedHeart == false) then
+		if (alreadyTouchedHeart == false and alreadyTouchedPlanet == false) then
 			alreadyTouchedStar = true
 		end
 	end
@@ -66,21 +72,49 @@ local function StarListener(touch)
 	if (touch.phase == "ended") then
 	   alreadyTouchedStar = false
 	   alreadyTouchedHeart = false
+	   alreadyTouchedPlanet = false
 	end
 end
 
+-- Function: Planetlistener
+-- Input: touch listener
+-- Output: none
+-- Description: moves the planet when touched by user
+local function PlanetListener(touch)
+
+	if (touch.phase == "began") then
+		if (alreadyTouchedStar == false and alreadyTouchedHeart == false) then
+			alreadyTouchedPlanet = true
+		end
+	end
+
+	if ( (touch.phase == "moved") and(alreadyTouchedPlanet == true) ) then
+		planet.x = touch.x
+		planet.y = touch.y
+	end
+
+	if (touch.phase == "ended") then
+	   alreadyTouchedHeart = false
+	   alreadyTouchedStar = false
+	   alreadyTouchedPlanet = false
+	end
+end
 -----------------------------------------------------------------------------------------
 
 -- hide the status bar
 display.setStatusBar(display.HiddenStatusBar)
 
--- sets the initial x and y positions of the heart and star
+-- sets the initial x and y positions of the heart, star, and planet
 heart.x = 400
 heart.y = 500
 
 star.x = 300
 star.y = 200
 
+planet.x = 350
+planet.y = 350
+
 -- adds the respective listeners per each object
 heart:addEventListener("touch", HeartListener)
 star:addEventListener("touch",  StarListener)
+planet:addEventListener("touch", PlanetListener) 
