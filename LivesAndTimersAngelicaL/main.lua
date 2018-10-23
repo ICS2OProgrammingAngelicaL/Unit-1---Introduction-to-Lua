ed1de61c5bb8c06ec5149364be9a966bf6cc6efb
AngelicaL714 <angelica.lutkiewicz@ocsbstudent.ca>
2018-10-23 14:28:21 -0400

    Timer still doesn't work and need to add sound

--- LivesAndTimersAngelicaL/main.lua
+++ LivesAndTimersAngelicaL/main.lua
@@ -23,14 +23,15 @@ local userAnswer
 local correctAnswer
 local randomOperator
 local tally = 0
-local totalSeconds = 5
-local secondsLeft = 5
 local clockText
 local countDownTimer
-local lives = 3
 local heart1
-local heart2
+local heart2 
 local heart3
+local heart4
+local lives = 4		
+local secondsLeft = 15
+local totalSeconds = 15
 
 
 -- Object creation
@@ -49,34 +50,52 @@ correctObject = display.newText("Correct :)", display.contentWidth/2, display.co
 correctObject:setTextColor(0,1,0)
 correctObject.isVisible = false
 
+clockText = display.newText("", display.contentWidth *10/12, display.contentHeight *11/12, nil, 50)
+clockText:setTextColor(1)
+
 -- Create numeric field
 numericField = native.newTextField( display.contentWidth/2, display.contentHeight *2.5/3, 800, 80 )
 numericField.inputType = "number"
 
 correctTally = display.newText("", display.contentWidth/6, display.contentHeight/6, nil, 50) 
 
-heart1 = display.newImageRect("Images/heart.png", 100, 100)
+heart1 = display.newImageRect("Images/heart.png", 75, 75)
 heart1.x = display.contentWidth *7/8
 heart1.y = display.contentHeight *1/7
+heart1.isVisible = true
 
-heart2 = display.newImageRect("Images/heart.png", 100, 100)
+heart2 = display.newImageRect("Images/heart.png", 75, 75)
 heart2.x = display.contentWidth *6/8
 heart2.y = display.contentHeight *1/7
+heart2.isVisible = true
 
-heart3 = display.newImageRect("Images/heart.png", 100, 100)
+heart3 = display.newImageRect("Images/heart.png", 75, 75)
 heart3.x = display.contentWidth *5/8
 heart3.y = display.contentHeight *1/7
+heart3.isVisible = true
+
+heart4 = display.newImageRect("Images/heart.png", 75, 75)
+heart4.x = display.contentWidth *4/8
+heart4.y = display.contentHeight *1/7
+heart4.isVisible = true
 
 -----------------------------------------------------------------------------------------
 
 -- Local functions
+
+local function StartTimer()
+	-- Create a countdown timer that loops infinitely
+	countDownTimer = timer.performWithDelay( 15000, UpdateTime, 0)
+end
+
+-- StartTimer()
 
 local function UpdateTime()
 	-- Decrement the number of seconds
 	secondsLeft = secondsLeft - 1
 
 	--Display the number of seconds left in the clock object
-	clockText.text = secondsLeft .. ""
+	clockText.text = "Time: " .. secondsLeft
 
 	if (secondsLeft == 0 ) then
 		-- Reset the number of seconds left
@@ -85,11 +104,7 @@ local function UpdateTime()
 
 		-- If there are no lives left, play a lose sound, show you a lose image and 
 		-- Cancel the timer and remove the third heart by making it invisible
-		if (lives == 2) then
-			heart2.isVisible = false
-		elseif (lives == 1) then
-			heart1.isVisible = false
-		end
+		UpdateHearts()
 
 			-- Call the function to ask a new question
 			AskQuestion()
@@ -97,11 +112,50 @@ local function UpdateTime()
 	end
 end
 
--- Function that calls the timer
-local function StartTimer()
-	-- Create a countdown timer that loops infinitely
-	countDownTimer = timer.performWithDelay( 1000, UpdateTime, 0)
+
+--	clockText.text = secondsLeft .. ""
+
+
+local function UpdateHearts()
+
+	if (lives == 4) then
+		heart1.isVisible = true
+		heart2.isVisible = true
+		heart3.isVisible = true
+		heart4.isVisible = true
+
+	elseif ( lives == 3) then
+		heart1.isVisible = true
+		heart2.isVisible = true
+		heart3.isVisible = true
+		heart4.isVisible = false
+
+	elseif ( lives == 2 ) then
+		heart1.isVisible = true
+		heart2.isVisible = true
+		heart3.isVisible = false
+		heart4.isVisible = false
+
+	elseif ( lives == 1 ) then
+		heart1.isVisible = true
+		heart2.isVisible = false
+		heart3.isVisible = false
+		heart4.isVisible = false
+
+	else --( lives == 0 )
+		heart1.isVisible = false
+		heart2.isVisible = false
+		heart3.isVisible = false
+		heart4.isVisible = false
+
+	end
 end
+
+-- Function that calls the timer
+--local function StartTimer()
+	-- Create a countdown timer that loops infinitely
+--	countDownTimer = timer.performWithDelay( 1000, UpdateTime, 0)
+--end
 
 local function AskQuestion()
 	-- Generate 2 random numbers between a max. and a min. number
@@ -163,6 +217,7 @@ local function NumericFieldListener( event )
 			timer.performWithDelay(1500, HideCorrect)
 			incorrectSoundChannel = audio.play(incorrectSound)
 			lives = lives - 1
+			UpdateHearts()
 		end
 
 		-- Clear text field
@@ -176,3 +231,5 @@ numericField:addEventListener( "userInput", NumericFieldListener )
 
 -- call the function to ask the question
 AskQuestion()
+UpdateTime()
+StartTimer()
