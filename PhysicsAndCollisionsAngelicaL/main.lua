9002de0ae3a1001370f015b554cc9e53b6f41079
AngelicaL714 <angelica.lutkiewicz@ocsbstudent.ca>
2018-10-25 13:22:19 -0400

    mathquiz

--- PhysicsAndCollisionsAngelicaL/main.lua
+++ PhysicsAndCollisionsAngelicaL/main.lua
@@ -1,7 +1,25 @@
 -----------------------------------------------------------------------------------------
---
--- main.lua
---
+-- Title: PhysicsAndColissions
+-- Name: Angelica L
+-- Course: ICS2O
+-- This program . . .
 -----------------------------------------------------------------------------------------
 
--- Your code here
+-- Hide the status bar
+display.setStatusBar(display.HiddenStatusBar)
+
+-----------------------------------------------------------------------------------------
+-- Local variables
+local physics = require("physics")
+-----------------------------------------------------------------------------------------
+
+-- Start physics
+physics.start()
+
+-----------------------------------------------------------------------------------------
+-- Object creation
+-----------------------------------------------------------------------------------------
+-- Ground
+local ground = display.newImage("Images/ground.png", 0, 0)
+ground.x = display.contentCenterX
+ground.y = display.contentHeight *12/12
